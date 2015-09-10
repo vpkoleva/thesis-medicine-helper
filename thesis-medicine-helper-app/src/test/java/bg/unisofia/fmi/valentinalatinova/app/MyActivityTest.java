2aacb71d1e779fe2a9786f6fbb34861bb9c45059
Valentina Koleva <v.p.koleva@gmail.com>
2015-09-10 18:48:37 +0300

    Add build-able and deployable application.

--- thesis-medicine-helper-app/src/test/java/bg/unisofia/fmi/valentinalatinova/app/MyActivityTest.java
+++ thesis-medicine-helper-app/src/test/java/bg/unisofia/fmi/valentinalatinova/app/MyActivityTest.java
@@ -5,34 +5,29 @@ import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
 /**
- * Unit test for simple App.
+ * Unit test for simple MyActivity.
  */
-public class AppTest 
-    extends TestCase
-{
+public class MyActivityTest extends TestCase {
     /**
      * Create the test case
      *
      * @param testName name of the test case
      */
-    public AppTest( String testName )
-    {
-        super( testName );
+    public MyActivityTest(String testName) {
+        super(testName);
     }
 
     /**
      * @return the suite of tests being tested
      */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
+    public static Test suite() {
+        return new TestSuite(MyActivityTest.class);
     }
 
     /**
      * Rigourous Test :-)
      */
-    public void testApp()
-    {
-        assertTrue( true );
+    public void testApp() {
+        assertTrue(true);
     }
 }
